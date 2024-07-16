e32c9736a1bee884c7d559ad25366f43ba021757
Zachery Converse <zacheryconverse@gmail.com>
2024-07-16 15:29:09 -0600

    feat: prepare app for heroku

--- src/config.js
+++ src/config.js
@@ -1,1 +1,7 @@
-export const API_ENDPOINT = 'http://localhost:8080'
+const LOCAL_API_ENDPOINT = 'http://localhost:8080'
+const PROD_API_ENDPOINT = 'https://vol-server-a7417ca800ec.herokuapp.com'
+
+export const API_ENDPOINT =
+  process.env.NODE_ENV === 'development'
+    ? LOCAL_API_ENDPOINT
+    : PROD_API_ENDPOINT
